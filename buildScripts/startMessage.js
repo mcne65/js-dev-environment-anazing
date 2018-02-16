08316bce53c48f9b05aa89db0e0fdfeeec14a424
Anai Araya <anai.yusary@gmail.com>
2018-02-16 17:57:00 +1100

    add eslint setup

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in dev mode Anai...'));
+console.log(chalk.green('Starting app in dev mode Anai...')); // eslint-disable-line no-console
 
