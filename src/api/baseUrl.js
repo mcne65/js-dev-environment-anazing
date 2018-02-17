11a97326795814c092034d516cbfe3dabd4d83ae
Anai Araya <anai.yusary@gmail.com>
2018-02-17 12:10:43 +1100

    Automated API deploy via Heroku

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName("useMockApi")
-    ? "http://localhost:3001/"
-    : "https://mysterious-dawn-16770.herokuapp.com/";
+  return getQueryStringParameterByName("useMockApi") ? "http://localhost:3001/" : "https://boiling-journey-13557.herokuapp.com/";
 }
 
 function getQueryStringParameterByName(name, url) {
